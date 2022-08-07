f1f6001bfd84dd75f345fb3c9360a7e61aa0746e
Vandan Adhvaryu <vandanadhvaryu99@gmail.com>
2022-08-07 13:53:15 +0100

    Add crop and compression

--- get_ptcg_cards.py
+++ get_ptcg_cards.py
@@ -6,13 +6,15 @@ import requests
 import os
 import logging
 import glob
+from PIL import Image
 from slpp import slpp
-
+from unidecode import unidecode
 
 class TCG:
-    def __init__(self, api_key: str, ptcg_map: dict, sets: dict = None):
+    def __init__(self, api_key: str, ptcg_map: dict, img_regions: dict = None, sets: dict = None):
         self.key_headers = {"x-api-key": api_key}
         self.ptcg_map = ptcg_map
+        self.img_regions = img_regions
         self.sets = sets if sets else {}  # limit api calls
         if not os.path.exists("sets"):
             os.makedirs("sets")
@@ -21,10 +23,36 @@ class TCG:
             sets = {st.split("\\")[-1].split(".")[0]: st for st in sets}
             self.sets = sets
     
-    def _make_lua_map(self, set_id, file_map):
+    @classmethod
+    def remove_non_ascii(cls, text):
+        sp_chars = {
+            " ♀": "F",
+            " ♂": "M",
+        }
+        for sp in sp_chars:
+            if text.endswith(sp):
+                text = text.replace(sp, sp_chars[sp])
+        return unidecode(text)
+
+    @classmethod
+    def _make_lua_map(cls, set_id, file_map):
         with open(f"lua/sets/{set_id}.lua", 'w+') as f:
             f.write(f"return {slpp.encode(file_map)}")
         return True
+
+    def _process_images(self, set_id, crop=True, quality=0):
+        files = glob.glob(f"sets/images/{set_id}/*.png")
+        for f in files:
+            supertype = f.split("_")[-1].split(".")[0]
+            img = Image.open(f)            
+            if crop and set_id in self.img_regions:
+                region = self.img_regions[set_id][supertype]
+                region = (region[0], region[1], region[0] + region[2], region[1] + region[3])
+                img = img.crop(region)    
+            if quality > 0:
+                img.save(f, 'JPEG', quality=quality)
+            else:
+                img.save(f)
 
     def _map(self, set_id: str):
         remaining_map = list(self.ptcg_map.values())
@@ -33,7 +61,7 @@ class TCG:
         for f in files:
             fname = f.split("\\")[-1]
             fname = fname.split("_", 1)[-1] if fname.count("_") > 1 else fname
-            pokemon = fname.split("_")[0].upper().replace(" ", "_")
+            pokemon = fname.split("_")[0].replace(" ", "_")
             if pokemon in self.ptcg_map:
                 pk = self.ptcg_map[pokemon]
                 mem_value = str(pk)
@@ -49,7 +77,7 @@ class TCG:
             logging.error(f"{set_id}: Unable to map: {remaining_map}.")
         return file_map
 
-    def save_images(self, set_id: str, start=None, end=None, hi_res=True):
+    def save_images(self, set_id: str, start=None, end=None, get_hi_res=True, compress=0):
         if not set_id in self.sets:
             return False
         dir = f"sets/images/{set_id}"
@@ -63,19 +91,19 @@ class TCG:
         end = len(set_data) if end is None else end
 
         for card in set_data[start:end]:
-            res = "large" if hi_res else "small"
+            card["supertype"] = TCG.remove_non_ascii(card['supertype'])
+            card["name"] = "".join([c for c in TCG.remove_non_ascii(card['name']).upper() if c == " " or (ord(c) >= 65 and ord(c) <= 90)])
+
+            res = "large" if get_hi_res else "small"
             url = card["images"][res]
             img = requests.get(url).content
-            card["supertype"] = (
-                "Pokemon"
-                if card["supertype"] not in ("Energy", "Trainer")
-                else card["supertype"]
-            )  # Pokémon replaced with Pokemon
+
             img_file = f"{dir}/{card['name']}_{card['supertype']}.png"
             with open(img_file, "wb+") as f:
                 f.write(img)
-
-        return self._make_lua_map(set_id, self._map(set_id))
+        if compress > 0 or set_id in self.img_regions:
+            self._process_images(set_id, True, compress)
+        return TCG._make_lua_map(set_id, self._map(set_id))
 
     def get(self, set_name: str, update=False):
         new_sets = (
@@ -123,12 +151,25 @@ class TCG:
 
 
 if __name__ == "__main__":
+    json_file_path = "regions.json"
+    with open(json_file_path, 'r') as j:
+        img_regions = json.loads(j.read())
+        
     from poketcg.tools.constants import cards
-
     cards = {v: k for k, v in cards.items()}
+    cards_base = {}
+    for k, v in cards.items():
+        if k[-1] in ("1", "2", "3"):
+            if k[-1] == "1":
+                k = k[:-1]
+            else:
+                continue            
+        cards_base[k] = v
+
     API_KEY = os.environ.get("POKEMONTCG_IO_API_KEY")
 
-    tcg = TCG(API_KEY, cards)
-    bases = tcg.get("base4")
+    tcg = TCG(API_KEY, cards_base, img_regions)
+
+    bases = tcg.get("base")
     for b in bases:
         tcg.save_images(b)
--- lua/tcg.lua
+++ lua/tcg.lua
@@ -11,15 +11,28 @@ while true do
 	wy = client.screenheight()
 	xdiff = 880
 	ydiff = 660
-	mem = memory.read_u8(52267)
-	if mem ~= prev_pokemon then
+	mem = tostring(memory.read_u8(52267))
+	clicked = input.getmouse()["Left"]
+	if clicked then
 		gui.clearGraphics()
-		gui.clearImageCache()
-		prev_pokemon = tostring(mem)	
+	elseif mem ~= prev_pokemon then
+		prev_pokemon = mem	
 		if mapfiles[prev_pokemon] ~= nil then
 			fname = "../"..mapfiles[prev_pokemon]
+			-- draw center:EMU
+			-- gui.drawImage(fname, client.bufferwidth()/3.25, client.bufferheight()/4.25, 62, 46)			
+
+			-- draw from full card:CLIENT
 			-- "Pokemon": [65, 100, 470, 328],
-			gui.drawImageRegion(fname, 65, 100, 470, 328, wx - xdiff, wy - ydiff, xdiff, ydiff)		
+			-- gui.drawImageRegion(fname, 65, 100, 470, 328, wx - xdiff, wy - ydiff, xdiff, ydiff)		
+
+			-- draw bottom right:CLIENT
+			-- gui.drawImage(fname, wx - xdiff, wy - ydiff, xdiff, ydiff)
+
+			-- draw center:CLIENT
+			gui.drawImage(fname, ((wx - client.borderwidth())/3.3) + client.borderwidth(), client.screenheight()/4.45, xdiff, ydiff)			
+		else
+			gui.clearGraphics()
 		end
 	end	
 		emu.frameadvance()
